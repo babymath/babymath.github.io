159dfd6235d75c6dcf3749c10ecaea349a8f206a
babymath <164469039+babymath@users.noreply.github.com>
2025-02-10 22:02:06 +0530

    Update about

---
+++ about/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const playButton = document.querySelector(".play-button");
+
+    playButton.addEventListener("click", function() {
+        alert("Get ready to play BabyMath!");
+    });
+});
