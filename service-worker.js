835f2155f616c6d9e2c328e7e558d7d9c847d18c
babymath <164469039+babymath@users.noreply.github.com>
2025-02-19 22:30:35 +0530

    Update

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = "babymath-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./aadhaar/code/style.css",
+  "./aadhaar/code/script.js",
+  "./aadhaar/img/icon-192.png",
+  "./aadhaar/img/icon-512.png"
+];
