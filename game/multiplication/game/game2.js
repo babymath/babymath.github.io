3f304627c7eea9047154e938b84d88dceb1fd36e
babymath <164469039+babymath@users.noreply.github.com>
2025-03-08 19:21:42 +0530

    Update game2.js

--- game/multiplication/game/game2.js
+++ game/multiplication/game/game2.js
@@ -69,12 +69,12 @@ function generateQuestion() {
     console.log('generateQuestion function called');
     const selectedQuestionType = document.querySelector('.typeOption.selected');
     let num1, num2;
-    if (selectedQuestionType.dataset.value === '2-2') {
-        num1 = Math.floor(Math.random() * 50 + 51); // num1 will be between 51 and 100
-        num2 = Math.floor(Math.random() * 10 + 49); // num2 will be between 10 and 99
-    } else if (selectedQuestionType.dataset.value === '3-3') {
-        num1 = Math.floor(Math.random() * 500 + 501); // num1 will be between 501 and 1000
-        num2 = Math.floor(Math.random() * 900 + 100); // num2 will be between 100 and 999
+    if (selectedQuestionType.dataset.value === '3×1') {
+        num1 = Math.floor(Math.random() * 900 + 100); // num1 will be between 100 and 999
+        num2 = Math.floor(Math.random() * 8 + 2); // num2 will be between 2 and 9
+    } else if (selectedQuestionType.dataset.value === '2×2') {
+        num1 = Math.floor(Math.random() * 90 + 10); // num1 will be between 10 and 99
+        num2 = Math.floor(Math.random() * 90 + 10); // num2 will be between 10 and 99
     } else {
         console.log('No question type is selected');
     }
