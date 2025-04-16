6131176b5ee938e284904a130e325d413cdef988
babymath <164469039+babymath@users.noreply.github.com>
2025-04-17 00:03:37 +0530

    Create subjectData.js

---
+++ subjectData.js
@@ -0,0 +1,17 @@
+        const subjectsData = {
+            Math: [
+                "lesson1",
+                "lesson2",
+                "lesson3"
+            ],
+            Science: [
+                "biology",
+                "chemistry",
+                "physics"
+            ],
+            History: [
+                "ancient",
+                "medieval",
+                "modern"
+            ]
+        };
